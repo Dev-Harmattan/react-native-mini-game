7329382a6751f09a894acc44edcdd750cb15585c
Sodiq Olalekan <ganiyusodiq059@gmail.com>
2022-10-25 04:33:30 -0600

    Initial layour and primary button component

---
+++ components/PrimaryButton.js
@@ -0,0 +1,45 @@
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+
+export const PrimaryButton = ({ children }) => {
+  const handleButtonPress = () => {
+    console.log('PRESS');
+  };
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        onPress={handleButtonPress}
+        android_ripple={{ color: '#640233' }}
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.iosRipple]
+            : styles.buttonInnerContainer
+        }
+        // style={(pressed) => pressed && ''}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+    // width: 120
+  },
+  buttonInnerContainer: {
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    elevation: 4,
+    backgroundColor: '#73063c',
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  iosRipple: {
+    opacity: 0.75,
+  },
+});
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,60 @@
+import { View, TextInput, StyleSheet } from 'react-native';
+import { PrimaryButton } from '../components/PrimaryButton';
+
+export const StartGameScreen = () => {
+  return (
+    <View style={[styles.inputContainer, styles.shadowProp]}>
+      <TextInput
+        style={styles.numberInput}
+        underlineColorAndroid="transparent"
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCorrect={false}
+        autoCapitalize="none"
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    padding: 16,
+    marginHorizontal: 24,
+    backgroundColor: '#4e0329',
+    borderRadius: 8,
+    elevation: 4,
+    alignItems: 'center',
+  },
+  shadowProp: {
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    marginVertical: 8,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#ddb52f',
+    textAlign: 'center',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex: 1,
+  }
+});
